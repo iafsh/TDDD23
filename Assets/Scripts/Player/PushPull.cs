b1906e5d87849d2bfccba11cd66433213257465d
Afshan <88891430+iafsh@users.noreply.github.com>
2023-09-07 16:56:25 +0200

    Update PullPush script

--- Assets/Scripts/Player/PushPull.cs
+++ Assets/Scripts/Player/PushPull.cs
@@ -1,18 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PushPull : MonoBehaviour
 {
-    // Start is called before the first frame update
+    #region Internal
+    private bool playerTouch=false;
+    [SerializeField] float pushSpeed;
+    #endregion
+
+    #region External
+
+    private Vector2 playerDirection;
+
+    #endregion
+
     void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.C) && playerTouch)
+        {
+            this.gameObject.GetComponent<Rigidbody2D>().velocity = playerDirection * pushSpeed;
+        }
+        
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            playerTouch = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.transform.tag == "Player")
+        {
+            playerTouch = true;
+        }
         
     }
 }
