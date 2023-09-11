c932bdb4eef1a7d7b738f4393d4cc49d1c5c33ff
Afshan <88891430+iafsh@users.noreply.github.com>
2023-09-11 17:54:36 +0200

    Add walking

--- Assets/Scripts/Player/Walk.cs
+++ Assets/Scripts/Player/Walk.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Walk : MonoBehaviour
 {
+    [SerializeField] private float Speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,14 @@ public class Walk : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.GetComponent<Rigidbody2D>().velocity = Vector2.right * Speed;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.GetComponent<Rigidbody2D>().velocity = Vector2.left * Speed;
+        }
     }
 }
