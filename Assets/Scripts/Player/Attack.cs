9832884839b3c75d0badd7e539070bb1876d7781
FrankYin <djmax96945147@outlook.com>
2023-09-22 14:28:27 -0700

    character press J to attack

---
+++ Assets/Scripts/Player/Attack.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    private Animator animator;
+    private float attackDuration = 0.16f;//According to the animation 
+    private Coroutine attackCoroutine;
+    private float cooldownTimer = Mathf.Infinity;
+    [SerializeField] public float attackCooldown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.J) && cooldownTimer > attackCooldown) {
+            startAttack();
+        }
+        cooldownTimer += Time.deltaTime;
+    }
+    private void startAttack()
+    {
+        animator.SetTrigger("attack");
+        cooldownTimer = 0;
+    }
+
+}
