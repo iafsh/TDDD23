26ea34eb0eeecf20ec5bf6accce5cd653d2125e6
Afshan <88891430+iafsh@users.noreply.github.com>
2023-10-16 16:07:33 +0200

    Update level1

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -1,3 +1,5 @@
+using System;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
@@ -12,14 +14,13 @@ public class CameraFollow : MonoBehaviour
     void FixedUpdate()
     {
         Vector3 desiredPosition = target.position ;
+        //float a=math.clamp(desiredPosition.x, Walls.bounds.min.x + 4, Walls.bounds.max.x - 4);
+        //print(a);
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         // Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothSpeed);
         transform.position = smoothedPosition + offset;
+        
     }
-
-    private bool isCameraPassingWalls()
-    {
-        //if(Walls.bounds.max.x)
-        return false;
-    }
+    
+    
 }
