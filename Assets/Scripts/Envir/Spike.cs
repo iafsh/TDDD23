84a9396f43f84ecb72d2389feba2db3f556dd0a2
Sadra Saderi <sadrassx@gmail.com>
2023-10-20 16:17:05 +0330

    Create spike prefab

---
+++ Assets/Scripts/Envir/Spike.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Spike : MonoBehaviour
+{
+    [SerializeField] bool retracted = false;
+    [SerializeField] float retractSpeed = 10f;
+
+    Vector2 startPosition = Vector2.zero;
+
+    void Start()
+    {
+        startPosition = transform.position;
+
+        if (retracted)
+        {
+            transform.position = new Vector2(startPosition.x, startPosition.y - 1);
+            GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (retracted)
+        {
+            transform.position = new Vector2(
+                transform.position.x,
+                math.lerp(
+                    startPosition.y - 1,
+                    transform.position.y,
+                    math.exp2(-retractSpeed * Time.fixedDeltaTime)
+                )
+            );
+        }
+        else
+        {
+            transform.position = new Vector2(
+                transform.position.x,
+                math.lerp(
+                    startPosition.y,
+                    transform.position.y,
+                    math.exp2(-retractSpeed * Time.fixedDeltaTime)
+                )
+            );
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    public void Activate()
+    {
+        retracted = false;
+        GetComponent<BoxCollider2D>().enabled = true;
+    }
+
+    public void Deactivate()
+    {
+        retracted = true;
+        GetComponent<BoxCollider2D>().enabled = false;
+    }
+}
